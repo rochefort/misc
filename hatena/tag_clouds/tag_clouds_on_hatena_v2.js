c447538a52626aab05a6a57db7d4d11db798d4d0
rochefort <terasawan@gmail.com>
2014-05-09 02:40:22 +0900

    Add tag cloud js v2
    v1だと偏りがあり過ぎてruby、rails、雑記ぐらいしか大きく表示されないので
    決めうちで20を超えるカテゴリ以上は大きく表示するように修正した。

---
+++ hatena/tag_clouds/tag_clouds_on_hatena_v2.js
@@ -0,0 +1,27 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
+// タグクラウドもどき
+(function($) {
+    $(function() {
+    var categories = $(".hatena-module-category ul li a");
+    categories.each(function() {
+      var elm = $(this);
+      var elmText = elm.text().trim();
+      var m = elmText.match(/(.+) \((\d+)\)/);
+      if (m) {
+        var category = m[1];
+        var count = parseInt(m[2]);
+        
+        if(count >= 150)      elm.css("font-size", "300%");
+        else if(count >= 100) elm.css("font-size", "250%");
+        else if(count >= 50)  elm.css("font-size", "200%");
+        else if(count >= 20)  elm.css("font-size", "150%");
+        else elm.css("font-size", "90%");
+        elm.text(category);
+        elm.attr('title', elmText);
+      };
+    });
+  });
+})(jQuery);
+</script>
+
