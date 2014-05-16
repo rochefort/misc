f9e493bd1d83b9fef10a3db6abbb043f96de84a4
rochefort <terasawan@gmail.com>
2014-05-17 02:33:59 +0900

    Invoke jquery

---
+++ snippets/chrome/invoke_jquery.js
@@ -0,0 +1,9 @@
+if (!window.jQuery) {
+    document.body.appendChild((function(){
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+        return s;
+    })());
+}
+
