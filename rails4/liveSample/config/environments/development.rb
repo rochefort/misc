14cd555d03635bee8f29b9403133dc39065e394c
rochefort <terasawan@gmail.com>
2014-05-31 06:55:57 +0900

    Add leak of develpment.rb

--- rails4/liveSample/config/environments/development.rb
+++ rails4/liveSample/config/environments/development.rb
@@ -6,8 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.middleware.delete ::Rack::Lock
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
