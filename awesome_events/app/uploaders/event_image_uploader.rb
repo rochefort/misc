3f0f8869f3ef33e6d7695db20250daacf84145b4
rochefort <terasawan@gmail.com>
2014-06-30 03:29:34 +0900

    6.9 Implement resize an image by mini_magick

--- awesome_events/app/uploaders/event_image_uploader.rb
+++ awesome_events/app/uploaders/event_image_uploader.rb
@@ -1,14 +1,15 @@
 # encoding: utf-8
 
 class EventImageUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+
+  process :resize_to_fit => [1130, 1130]
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -38,9 +39,9 @@ class EventImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
