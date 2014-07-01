11691ced7e6940aeb29168cf9eb8cf8c1735fa2b
rochefort <terasawan@gmail.com>
2014-07-02 00:31:16 +0900

    7.4 add factory_girl_rails

--- awesome_events/spec/models/event_spec.rb
+++ awesome_events/spec/models/event_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Event, :type => :model do
     it { should validate_presence_of(:name) }
     it { should ensure_length_of(:name).is_at_most(50) }
   end
+
+  describe '#created_by?' do
+    let(:event) { create(:event) }
+    subject { event.created_by?(user) }
+
+    context '引数が nil なとき' do
+      let(:user) { nil }
+      it { should be_falsey }
+    end
+
+    context '#owner_id と 引数の#id が同じとき' do
+      let(:user) { double('user', id: event.owner_id) }
+      it { should be_truthy }
+    end
+
+    context '#owner_id と 引数の#id が異なるとき' do
+      let(:user) { double('user', id: event.owner_id + 1) }
+      it { should be_falsey }
+    end
+  end
 end
