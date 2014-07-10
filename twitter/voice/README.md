ab6738d65c7d5da882c4c4a76e349c06a8f53cef
rochefort <terasawan@gmail.com>
2014-07-11 01:39:04 +0900

    Implement sample using Voice API
    e.g.: http://dev.classmethod.jp/client-side/voicetext-web-api-read-twitter/

---
+++ twitter/voice/README.md
@@ -0,0 +1,1 @@
+[VoiceText Web APIを使ってTwitterのhome_timelineを読み上げる ｜ Developers.IO](http://dev.classmethod.jp/client-side/voicetext-web-api-read-twitter/)
---
+++ twitter/voice/voice.rb
@@ -0,0 +1,48 @@
+require 'twitter'
+require 'voice_text_api'
+require 'open3'
+require 'yaml'
+
+class VoiceSample
+  SPEAKER = [:haruka, :hikari, :takeru]
+
+  def speak
+    settings = YAML.load_file('settings.yml')
+    client = Twitter::REST::Client.new(settings['twitter'])
+    voice = VoiceTextAPI.new(settings['voice_api_key'])
+
+    client.home_timeline({count: 200}).each do |tweet|
+      speaker = SPEAKER[rand(3)]
+      text = tweet.text
+      emolevel =  text.match(/[!！]/) ? 2 : 1
+      emotion = distinguish_emotion(text)
+
+      if emotion
+        wav = voice.tts(text, speaker, emotion: emotion, emotion_level: emolevel)
+      else
+        wav = voice.tts(text, speaker)
+      end
+      Open3.capture3("#{settings['sox_path'] + '/play -'}", stdin_data: wav)
+    end
+  rescue
+    puts "RuntimeError: #{$!}"
+  end
+
+  private
+    def distinguish_emotion(text)
+      case text
+      when /[喜嬉楽幸]/
+        :happiness
+      when /[悲辛苦]/
+        :sadness
+      when /[怒]/
+        :anger
+      else
+        nil
+      end
+    end
+end
+
+if $0 == __FILE__
+  VoiceSample.new.speak
+end
