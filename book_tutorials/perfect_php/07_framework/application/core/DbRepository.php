951b6e84c3b74d1f4cbbeca1a39b47aa637fb89f
rochefort <terasawan@gmail.com>
2014-08-07 21:53:33 +0900

    7.2.18 add dbRepository class

---
+++ book_tutorials/perfect_php/07_framework/application/core/DbRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class DbRepository
+{
+    protected $con;
+
+    public function __construct($con)
+    {
+        $this->setConnection($con);
+    }
+
+    public function seetConnection($con)
+    {
+        $this->con = $con;
+    }
+
+    public function execute($sql, $params = array())
+    {
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    public function fetch($sql, $params = array())
+    {
+        return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
