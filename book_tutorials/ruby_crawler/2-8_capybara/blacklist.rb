aee9dd985f9d25afca656e36b0f4589a67f65e34
rochefort <terasawan@gmail.com>
2014-09-11 01:34:25 +0900

    2-8-4 Add caputuring blacklist

---
+++ book_tutorials/ruby_crawler/2-8_capybara/blacklist.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'capybara'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+
+# Capybara.run_server = false
+Capybara.current_driver = :selenium
+Capybara.app_host = 'http://www.blacklistalert.org'
+Capybara.default_wait_time = 20
+
+module Crawler
+  class BlacklistAlert
+    include Capybara::DSL
+
+    def domain_check(target)
+      visit('/')
+      fill_in 'q', with: target
+      click_button 'check'
+
+      within(:xpath, '/html/body/center/font/form') do
+        alert_notice if page.has_content?('Listed!')
+      end
+      page.save_screenshot('screenshot.png')
+    end
+
+    def alert_notice
+      puts 'Alert!!'
+    end
+  end
+end
+
+crawler = Crawler::BlacklistAlert.new
+crawler.domain_check('mail.google.com')
