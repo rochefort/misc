8dd5264be465922f0c7f93011b9611cd0531ea49
rochefort <terasawan@gmail.com>
2014-09-10 23:24:01 +0900

    2-5-2 Add narrowed by url

---
+++ book_tutorials/ruby_crawler/2-5_basic_crawler/just_crawling2.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'anemone'
+
+urls = [
+  'http://www.amazon.co.jp/gp/bestsellers/books/',
+  'http://www.amazon.co.jp/gp/bestsellers/digital-text/2275256051'
+]
+
+Anemone.crawl(urls, depth_limit: 1, skip_query_strings: true) do |anemone|
+  anemone.focus_crawl do |page|
+    page.links.keep_if { |link| link.to_s.match(%r{/gp/bestsellers/books|/gp/bestsellers/digital-text/}) }
+  end
+
+  pattern = %r[466298\/+|466282\/+|2291657051\/+|2291905051\+]
+  anemone.on_pages_like(pattern) { |page| puts page.url }
+end
