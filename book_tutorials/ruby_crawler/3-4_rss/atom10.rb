47b4643ebf100543ee41a8ca6b0ac21b3533af44
rochefort <terasawan@gmail.com>
2014-09-11 03:35:19 +0900

    Add atom

---
+++ book_tutorials/ruby_crawler/3-4_rss/atom10.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'nokogiri'
+require 'open-uri'
+
+url = 'http://b.hatena.ne.jp/dkfj/atomfeed'
+xml = open(url).read
+
+doc = Nokogiri::XML(xml)
+
+namespaces = {
+  'atom' => 'http://purl.org/atom/ns#',
+  'dc' => 'http://purl.org/dc/elements/1.1/'
+}
+
+entries = doc.xpath('//atom:entry', namespaces)
+entries.each do |entry|
+  puts entry.xpath('atom:title', namespaces).text
+end
