7c93ba8a00ab0bebf5f106d62a7d68740b7bb658
rochefort <terasawan@gmail.com>
2014-09-13 03:08:23 +0900

    Add anemone pstore example

---
+++ book_tutorials/ruby_crawler/4-1_anemone_storage/anemone_file.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# -*- coding: UTF-8 -*-
+require 'anemone'
+
+urls = ['http://www.yahoo.co.jp']
+
+opts = {
+  storage: Anemone::Storage.PStore('file.txt'),
+  obey_robots_txt: true,
+  depth_limit: 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath('/head/title/text()').to_s if page.doc
+  end
+end
