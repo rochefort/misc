b551cacc0f722e3d8641c11bb6b029274a0e4e0b
rochefort <terasawan@gmail.com>
2014-09-13 03:16:34 +0900

    Add anemone sqlite example

---
+++ book_tutorials/ruby_crawler/4-1_anemone_storage/anemone_sqlite.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# -*- coding: UTF-8 -*-
+require 'anemone'
+
+urls = ['http://www.yahoo.co.jp']
+
+opts = {
+  storage: Anemone::Storage::SQLite3(),
+  obey_robots_txt: true,
+  depth_limit: 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath('//title/text()').to_s if page.doc
+  end
+end
