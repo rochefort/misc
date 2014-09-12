07320e80bf7d47cc26fd58089dcad6ae8dd71a43
rochefort <terasawan@gmail.com>
2014-09-13 03:19:16 +0900

    Add anemone mongodb example

---
+++ book_tutorials/ruby_crawler/4-1_anemone_storage/anemone_mongo.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# -*- coding: UTF-8 -*-
+require 'anemone'
+
+urls = ['http://www.yahoo.co.jp']
+
+opts = {
+  storage: Anemone::Storage::MongoDB(),
+  depth_limit: 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath('//title/text()').to_s if page.doc
+  end
+end
