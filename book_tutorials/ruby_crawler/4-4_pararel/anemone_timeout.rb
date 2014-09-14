3782834a8aa83c714c5042be05b7246f2bfa652a
rochefort <terasawan@gmail.com>
2014-09-15 02:27:48 +0900

    Add anemone timeout example

---
+++ book_tutorials/ruby_crawler/4-4_pararel/anemone_timeout.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'anemone'
+
+urls = ['http://www.yahoo.co.jp']
+
+opts = {
+  obey_robots_txt: true,
+  read_timeout: 5,
+  depth_limit: 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath('//title/text()').to_s if page.doc
+  end
+end
