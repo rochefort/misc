8974d3fd0d47203713c83422213297d6c3719859
rochefort <terasawan@gmail.com>
2014-09-14 23:03:10 +0900

    Add anemone with thread example

---
+++ book_tutorials/ruby_crawler/4-4_pararel/anemone_thread.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'anemone'
+
+urls = [
+  'http://www.amazon.co.jp/gp/bestsellers/books/466284/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/571582/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/492152/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/466286/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/466282/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/492054/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/466290/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/492166/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/466298/',
+  'http://www.amazon.co.jp/gp/bestsellers/books/466294/'
+]
+
+opts = {
+  obey_robots_txt: true,
+  thread: 10,
+  depth_limit: 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath('//title/text()').to_s if page.doc
+  end
+end
