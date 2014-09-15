72d2c8f86bdcf56cb61384d3d55760b367ae41b7
rochefort <terasawan@gmail.com>
2014-09-15 19:34:20 +0900

    Add nokogiri extracting example

---
+++ book_tutorials/ruby_crawler/5-2_blog/nokogiri_extract.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'open-uri'
+require 'nokogiri'
+
+html = open('http://blog.takuros.net/entry/20140104/1388788175').read
+doc = Nokogiri::HTML(html)
+puts doc.xpath("//div[@class='entry-content']").text
