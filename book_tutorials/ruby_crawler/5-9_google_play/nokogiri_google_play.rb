88afa4d5925171df99773eb9a36accf88a762805
rochefort <terasawan@gmail.com>
2014-09-17 03:46:10 +0900

    Add nokogiri google play ranking example

---
+++ book_tutorials/ruby_crawler/5-9_google_play/nokogiri_google_play.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'nokogiri'
+
+url = 'https://play.google.com/store/apps/category/FINANCE/collection/topselling_paid'
+doc = Nokogiri::HTML(open(url), nil, 'UTF-8')
+doc.xpath("//h2/a[@class='title']").each { |item| puts item[:title] }
