12672fc7aff0652f166a685be9cba2206b684508
rochefort <terasawan@gmail.com>
2014-09-27 05:50:36 +0900

    #10 一覧（router/title_for_layout）

--- dotinstall/cakephp/blog/app/Controller/PostsController.php
+++ dotinstall/cakephp/blog/app/Controller/PostsController.php
@@ -5,5 +5,6 @@ class PostsController extends AppController {
 
     public function index() {
         $this->set('posts', $this->Post->find('all'));
+        $this->set('title_for_layout', '記事一覧');
     }
 }
