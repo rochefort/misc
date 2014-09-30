83b57f45de778e5a5c104b4d5ec6f7f953196981
rochefort <terasawan@gmail.com>
2014-10-01 02:02:19 +0900

    #16 model validation

--- dotinstall/cakephp/blog/app/Model/Post.php
+++ dotinstall/cakephp/blog/app/Model/Post.php
@@ -1,5 +1,13 @@
 <?php
 
 class Post extends AppModel {
-
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty',
+            'message' => '空じゃダメだし'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+        )
+    );
 }
