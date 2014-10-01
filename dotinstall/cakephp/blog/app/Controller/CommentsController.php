0c244df710bffeb5faf318fd788556915854eeef
rochefort <terasawan@gmail.com>
2014-10-02 00:29:41 +0900

    #26-30 listing and adding comments

---
+++ dotinstall/cakephp/blog/app/Controller/CommentsController.php
@@ -0,0 +1,33 @@
+<?php
+
+class CommentsController extends AppController {
+    public $helpers = array('Html', 'Form');
+
+    public function add() {
+      if ($this->request->is('post')) {
+        if ($this->Comment->save($this->request->data)) {
+          $this->Session->setFlash('success!');
+          $this->redirect(array('controller' => 'posts', 'action' => 'view', $this->data['Comment']['post_id']));
+        } else {
+            $this->Session->setFlash('failed!');
+        }
+      }
+    }
+
+    // public function delete($id) {
+    //     if ($this->request->is('get')) {
+    //         throw new MethodNotAllowedException();
+    //     }
+    //     if ($this->request->is('ajax')) {
+    //         if ($this->Post->delete($id)) {
+    //             $this->autoRender = false;
+    //             $this->autoLayout = false;
+    //             $response = array('id' => $id);
+    //             $this->header('Content-Type: application/json');
+    //             echo json_encode($response);
+    //             exit();
+    //         }
+    //     }
+    //     $this->redirect(array('action' => 'index'));
+    // }
+}
