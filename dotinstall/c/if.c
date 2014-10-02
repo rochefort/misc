77510a73f951dc7854da0cb82578a98422c3c8ce
rochefort <terasawan@gmail.com>
2014-10-03 01:50:15 +0900

    #5 if

---
+++ dotinstall/c/if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int score = 50;
+  if(score >= 60) {
+    printf("OK\n");
+  } else if(score >= 50) {
+    printf("so so... \n");
+  } else {
+    printf("NG!\n");
+  }
+  return 0;
+}
