3a9bb4a65d9f8854296471c31957e66e9f9fa1a1
rochefort <terasawan@gmail.com>
2014-10-03 01:54:12 +0900

    Rename files

---
+++ dotinstall/c/03_vaiable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+変数：データにつけるラベル
+データ型
+int型（整数） %d
+float型（実数） %f
+char（1文字） %c
+*/
+int main(void) {
+  int x = 10;
+  float y = 5.2;
+  char c = 'A';
+
+  printf("x = %d, y = %f, c = %c\n", x, y , c);
+  return 0;
+}
