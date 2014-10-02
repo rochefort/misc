fc68745e6a635092a3dc90501a919942a1639216
rochefort <terasawan@gmail.com>
2014-10-03 00:56:58 +0900

    #8 layout.erb

--- dotinstall/sinatra/main.rb
+++ dotinstall/sinatra/main.rb
@@ -2,5 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  @title = 'main index'
+  @content = 'main content'
   erb :index
+end
+
+get '/about' do
+  @title = 'about this page'
+  @content = 'about content'
+  @email = 'taguchi@gmail.com'
+  erb :about
 end
