ffd3316313ce3f9eb30fbe0a9157ac240ecce6e5
rochefort <terasawan@gmail.com>
2014-10-03 01:03:33 +0900

    #09 filter

--- dotinstall/sinatra/main.rb
+++ dotinstall/sinatra/main.rb
@@ -1,15 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+before '/admin/*' do
+  @author = 'admin area!'
+end
+
+before do
+  @author = 'taguchi'
+end
+
+after do
+  logger.info 'page displayed successfully'
+end
+
 get '/' do
   @title = 'main index'
-  @content = 'main content'
+  @content = 'main content' + @author
   erb :index
 end
 
 get '/about' do
   @title = 'about this page'
-  @content = 'about content'
+  @content = 'about content' + @author
   @email = 'taguchi@gmail.com'
   erb :about
 end
