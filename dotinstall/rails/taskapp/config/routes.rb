8821652ebc0c16d9f63d9f7a7256bcda5c001be7
rochefort <terasawan@gmail.com>
2015-01-05 17:05:00 +0900

    rails tutorial done

--- dotinstall/rails/taskapp/config/routes.rb
+++ dotinstall/rails/taskapp/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root 'projects#index'
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
+
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
