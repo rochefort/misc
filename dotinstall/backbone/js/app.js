70c1c022406caa7b7cdc075a9f56213a55dd0ab4
rochefort <terasawan@gmail.com>
2015-01-05 17:22:55 +0900

    backbone#04 get/set

--- dotinstall/backbone/js/app.js
+++ dotinstall/backbone/js/app.js
@@ -4,10 +4,21 @@ var Task = Backbone.Model.extend({
   defaults: {
     title: 'do something!',
     completed: false
+  },
+  toggle: function() {
+    this.set('completed', !this.get('completed'));
   }
 });
+
 var task1 = new Task({
   completed: true
 });
+
+// task1.set('title', 'newTitle');
+// var title = task1.get('title');
+// console.log(title);
+
+console.log(task1.toJSON());
+task1.toggle();
 console.log(task1.toJSON());
 })();
