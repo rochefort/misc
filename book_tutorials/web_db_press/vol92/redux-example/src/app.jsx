160d63a89fbf6255f192f7fb435934bf55b71294
rochefort <terasawan@gmail.com>
2017-03-28 11:43:40 +0900

    Add counter for react-example by react-redux

--- book_tutorials/web_db_press/vol92/redux-example/src/app.jsx
+++ book_tutorials/web_db_press/vol92/redux-example/src/app.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+
+// Action Creators
+function incrementCounter() {
+  return { type: 'INCREMENT_COUNTER' }
+}
+
+function decrementCounter() {
+  return { type: 'DECREMENT_COUNTER' }
+}
+
+// Reducer
+function counter(state = {counter: 0}, action) {
+  switch (action.type) {
+  case 'INCREMENT_COUNTER':
+    return {counter: state.counter + 1}
+  case 'DECREMENT_COUNTER':
+    return {counter: state.counter - 1}
+  default:
+    return state
+  }
+}
+
+// React Component
+let App = ({counter, dispatch}) => (
+  <div>
+    <div>{counter}</div>
+    <button onClick={() => dispatch(incrementCounter())}>+</button>
+    <button onClick={() => dispatch(decrementCounter())}>-</button>
+  </div>
+)
+
+// ReactとReduxの接続
+function mapStateToProps(state) {
+  return {counter: state.counter}
+}
+App = connect(mapStateToProps)(App)
+
+const store = createStore(counter, {counter: 0})
 
 render (
-  <h1>Hello, Redux</h1>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('container')
 )
