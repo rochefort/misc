d21360bbfabc676fbd5848bcea132e514b16894c
rochefort <terasawan@gmail.com>
2017-04-12 00:30:09 +0900

    Add base codes webdb97 react-spa

---
+++ book_tutorials/web_db_press/vol97/react_spa/spa-note/memo.md
@@ -0,0 +1,7 @@
+npm install --save react react-dom
+npm install --save-dev webpack@1.13.3
+npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
+npm install --save-dev webpack-dev-server@1.16.2
+
+./node_modules/.bin/webpack
+./node_modules/.bin/webpack-dev-server
---
+++ book_tutorials/web_db_press/vol97/react_spa/spa-note/webpack.config.js
@@ -0,0 +1,34 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    js: './src/main.js',
+    css: './src/main.css'
+  },
+  output: { path: './public', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader!postcss-loader')
+      }
+    ]
+  },
+  plugins: {
+    new ExtractTextPlugin('bundle.css');
+  },
+  postcss: {
+    require('postcss-easy-import')({ glob: true })
+  },
+  devServer: {
+    contentBase: './public',
+    port: 8080,
+    inline: true,
+    historyApiFallback: true
+  }
+}
